405494e94e7244166c4681c1fedf4919dfc23bd0
ABC-0099 <seonghoi347@gmail.com>
2026-01-19 17:33:44 +0900

    nice

--- front/Java/navigation.js
+++ front/Java/navigation.js
@@ -3,6 +3,7 @@ const toggleBtn = document.querySelector(".navbar-toggleBtn");
 const menu = document.querySelector(".navbar-menu");
 const icons = document.querySelector(".navbar-icons");
 
+// 바로 아래가 이벤트 처리 코드
 toggleBtn.addEventListener("click", (e) => {
   // 기본 동작(링크 이동) 방지
   e.preventDefault();
