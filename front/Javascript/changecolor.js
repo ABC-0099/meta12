29c47b6af509a5771b65558d2ebe8da169671f03
ABC-0099 <seonghoi347@gmail.com>
2026-08-11 16:56:35 +0900

    Sungho

---
+++ front/Javascript/changecolor.js
@@ -0,0 +1,11 @@
+let heading = document.getElementById('heading');
+heading.onclick = function()
+{
+    heading.style.color = "red";
+}
+
+// alert("안녕?")
+// let reply = confirm("정말 바꾸시겠습니까?");
+// let name = prompt("이름 입력","홍길동");
+// var name = prompt("이름 입력");
+document.write("<h1>하이</h1>");
